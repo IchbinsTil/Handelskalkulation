60ff702d3a93c81c2241d628cb0af1306e7c6ee5
IchbinsTil <tilsieper07@gmail.com>
2023-09-22 11:42:38 +0200

    Kommentar hinzugefügt

--- handelskalkulation/Program.cs
+++ handelskalkulation/Program.cs
@@ -9,7 +9,7 @@ namespace handelskalkulation
     static class Program
     {
         /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// Der Haupteinstiegspunkt für die Anwendung. 
         /// </summary>
         [STAThread]
         static void Main()
--- handelskalkulation/frm_handelskalkulation.Designer.cs
+++ handelskalkulation/frm_handelskalkulation.Designer.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// Verwendete Ressourcen bereinigen.
         /// </summary>
-        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>yxcfefgfegdfgbgwegtreferqwfrgergtzhgwfghtftzjhewhtgftzhr
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
